428e9a2e33f61746382632c4eee88fd81c5beab5
Gabriel Rosa Travassos <146151773+Gabrielrtr@users.noreply.github.com>
2025-03-01 22:38:11 +0000

    mensagem

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return(
+    <div>
+        <h1>37 Respondeu Jesus: " ‘Ame o Senhor, o seu Deus de todo o seu coração, de toda a sua alma e de todo o seu entendimento’.</h1>
+        <h1>38 "Este é o primeiro e maior mandamento.</h1>
+        <h1>39 E o segundo é semelhante a ele: ‘Ame o seu próximo como a si mesmo’</h1>
+    </div> 
+    )
+
+}
+ls -l
+
+export default Home;
